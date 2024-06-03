0ad1be6f4d6a3cbabfce4eef73e190223c3c7ddb
Faridrhmn <bfaridrahman@gmail.com>
2024-06-04 04:40:46 +0700

    fix error

--- view/admin.php
+++ view/admin.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 session_start();
 
 if (empty($_SESSION['status'])) {
@@ -36,7 +36,7 @@ $penerbits = isset($penerbitsResponse['data']) ? $penerbitsResponse['data'] : []
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Admin Panel</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
@@ -132,7 +132,7 @@ $penerbits = isset($penerbitsResponse['data']) ? $penerbitsResponse['data'] : []
                 })
                 .then(response => response.json())
                 .then(data => {
-                    if (data.success) {
+                    if (data.status === 'success' && !data.error) {
                         alert('Buku berhasil dihapus!');
                         location.reload();
                     } else {
@@ -153,7 +153,7 @@ $penerbits = isset($penerbitsResponse['data']) ? $penerbitsResponse['data'] : []
                 })
                 .then(response => response.json())
                 .then(data => {
-                    if (data.success) {
+                    if (data.status === 'success' && !data.error) {
                         alert('Penerbit berhasil dihapus!');
                         location.reload();
                     } else {
